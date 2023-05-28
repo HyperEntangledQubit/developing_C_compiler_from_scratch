b3a84d528bdd572aede770e07fbb52afa2a40b1c
Brad <25336193+HyperEntangledQubit@users.noreply.github.com>
2023-05-28 17:28:35 -0400

    Added utils dir to hold to files for vector and buffer classes. Adding gitignore

--- heq/main.c
+++ heq/main.c
@@ -1,6 +1,19 @@
 #include<stdio.h>
 
+#include "utils/vector.h"
+#include "compiler.h"
+
 int main(int argc, char* argv[]) {
-    printf("Hello, World from HEQ compiler\n");
+
+    int result = compile_file("dummy.c", "dummy.c", 0);
+
+    if (result == COMPILER_FILE_COMPILED_OK) {
+        printf("HEQ compiler succeeded compilation!\n");
+    } else if (result == COMPILER_FAILED_WITH_ERRORS){
+        printf("HEQ compiler failed compilation!\n");
+    } else {
+        printf("HEQ compiler received an unknown response!\n");
+    }
+
     return 0;
 }
