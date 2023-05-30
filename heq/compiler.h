49112b8dbf121d3e123439faaecbf82dad5ee06f
Brad <25336193+HyperEntangledQubit@users.noreply.github.com>
2023-05-30 14:50:04 -0400

    Adding token structs

--- heq/compiler.h
+++ heq/compiler.h
@@ -4,6 +4,40 @@
  * Not the cleanest way to do this. Right now, we are optimizing for ease. */
 
 #include <stdio.h> // Put this into the Makefile
+#include <stdbool.h>
+
+struct pos {
+    int line;
+    int col;
+    const char* filename;
+};
+
+enum {
+    TOKEN_TYPE_IDENTIFIER,
+    TOKEN_TYPE_KEYWORD,
+    TOKEN_TYPE_OPERATOR,
+    TOKEN_TYPE_SYMBOL,
+    TOKEN_TYPE_NUMBER,
+    TOKEN_TYPE_STRING,
+    TOKEN_TYPE_COMMENT,
+    TOKEN_TYPE_NEWLINE,
+};
+
+struct token {
+    int type;
+    int flags;
+
+    union {
+        char cval;
+        const char* sval;
+        unsigned int inum;
+        unsigned long lnum;
+        unsigned long long llnum;
+        void* any;
+    };
+    bool whitespace; // only true if whitespace between multiple token(s)
+    const char* between_brackets; // points to all tokens inside brackets
+};
 
 enum {
     COMPILER_FILE_COMPILED_OK,
